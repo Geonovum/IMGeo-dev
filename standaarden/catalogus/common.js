31c57a7e89b79190234c1fb29ba69515832bfe87
Arnoud de Boer <a.deboer@geonovum.nl>
2017-09-04 12:35:00 +0200

    Create common.js

---
+++ standaarden/catalogus/common.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  publishDate: "2017-07-28",
+  editors: [{
+    name: "First Lastname",
+    company: "Company",
+    companyURL: "CompanyURL",
+  },{
+    name: "Second Lastname",
+    company: "Company",
+    companyURL: "CompanyURL",
+  }],
+  shortName: "bro-bhr",
+  github: "https://github.com/adbgnm/IMGeo-spiegel",
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
