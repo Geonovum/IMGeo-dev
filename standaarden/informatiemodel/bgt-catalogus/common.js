dc16d68b89c7e3391c3b0b05aca006440c8b7f83
Arnoud de Boer <a.deboer@geonovum.nl>
2017-09-12 14:19:27 +0200

    Create common.js

---
+++ standaarden/informatiemodel/bgt-catalogus/common.js
@@ -0,0 +1,13 @@
+var respecConfig = {
+        specStatus: "ED",
+        editors: [{
+          name: "Arnoud de Boer, Geonovum",
+          url: "http://www.geonovum.nl",
+        }, {
+          name: "Hans van Eekelen, Geonovum",
+          url: "http://www.geonovum.nl"
+        }],
+        processVersion: 2017,
+        edDraftURI: "https://www.github.com/Geonovum/IMGeo2018",
+        shortName: "IMGeo2018"
+};
