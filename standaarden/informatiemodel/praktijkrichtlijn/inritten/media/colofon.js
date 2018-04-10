c165148f18110a23d62a1c40e8962af947b2c238
Arnoud de Boer <AdeBoer@geonovum.nl>
2018-04-10 10:17:39 +0200

    UPDATE COLOFON

--- standaarden/informatiemodel/praktijkrichtlijn/inritten/media/colofon.js
+++ standaarden/informatiemodel/praktijkrichtlijn/inritten/media/colofon.js
@@ -5,14 +5,14 @@ var respecConfig = {
   publishDate: "2018-04-10",
   editors: [
     {
-      name: "Arnoud de Boer",
+      /*name: "Arnoud de Boer",*/
       company: "Geonovum",
       companyURL: "http://www.geonovum.nl/",
       mailto: "a.deboer@geonovum.nl",
       note: ""    
     },  
 	{
-      name: "Hans van Eekelen",
+      /*name: "Hans van Eekelen",*/
       company: "Geonovum",
       companyURL: "http://www.geonovum.nl/",
       mailto: "p.janssen@geonovum.nl",
@@ -20,18 +20,7 @@ var respecConfig = {
     },  
   ],
   shortName: "IMGeo2018",
-  pubDomain: "IMGeo2018", // Aanpassen na verplaatsing naar nieuwe Github repos 
-  //previousURI: "https://www.geonovum.nl/BRO",
+  pubDomain: "IMGeo2018",
   github: "https://github.com/Geonovum/IMGeo2018",
-  previousPublishDate: "1900-01-01",
-  //previousStatus: "GN-WV",
-  //localBiblio: {
-  //  "ISO19136": {
-  //    "href": "https://www.iso.org/standard/32554.html",
-  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
-  //    "authors": [""],
-  //    "date": "September 2007",
-  //    "publisher": "International Organization for Standardization",
-  //  },
-  //},
+  
 };
