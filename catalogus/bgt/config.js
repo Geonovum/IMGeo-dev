72c2c576a87ba7ca68ee2ec2b7232bcc325e6ffe
Arnoud de Boer <a.deboer@geonovum.nl>
2020-06-24 11:25:43 +0200

    Update config.js

--- catalogus/bgt/config.js
+++ catalogus/bgt/config.js
@@ -1,35 +1,21 @@
 var respecConfig = {
   format: "markdown",
-  specStatus: "GN-WV",
+  specStatus: "GN-VV",
   specType: "IM",
-  subtitle: "Gegevenscatalogus BGT 1.2",
+  //subtitle: "Gegevenscatalogus BGT 1.2",
   //canonicalURI: "WV",
   //publishDate: "2017-09-12",
-  editors: [
+  authors: [
     {
-      name: "Arnoud de Boer",
-      company: "Geonovum",
+      name: "Geonovum",
       companyURL: "http://www.geonovum.nl/",
-      mailto: "a.deboer@geonovum.nl",
-      note: ""    
-    },	
-    {
-      name: "Hans van Eekelen",
-     company: "Geonovum",
-    companyURL: "http://www.geonovum.nl/",
-    mailto: "h.vaneekelen@geonovum.nl",
-    note: "" 
-    },
-	{name: "Silvy Horbach",
-     company: "SVB-BGT",
-    companyURL: "http://www.svb-bgt.nl/",
-    mailto: "silvyhorbach@svb-bgt.nl",
-    note: ""
-	}	
+      mailto: "imgeo@geonovum.nl",
+       
+    }   
   ],
   //shortName: "shortname",
   pubDomain: "http://www.geonovum.nl/wegwijzer/standaarden/gegevenscatalogus-bgt-111",
-  github: "https://github.com/Geonovm/IMGeo2018",
+  github: "https://github.com/Geonovm/IMGeo",
   //previousPublishDate: "2017-01-01",
   //previousStatus: "GN-CV",
   //localBiblio: {
