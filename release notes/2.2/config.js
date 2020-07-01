6a9bd7870b972a8f5302fd97dff1564247eae1f6
Arnoud de Boer <a.deboer@geonovum.nl>
2020-07-01 16:06:06 +0200

    Create config.js

---
+++ release notes/2.2/config.js
@@ -0,0 +1,16 @@
+var respecConfig = {
+  specStatus: "GN-DEF",
+  specType: "AL",  
+  editors: [
+    {
+      name: "Geonovum",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/",
+      mailto: "imgeo@geonovum.nl",
+      note: ""    
+    }
+  ],
+  shortName: "imgeo",
+  pubDomain: "IMGeo",
+  github: "https://github.com/Geonovum/IMGeo-dev", 
+};
